cbffc0b09320883eb85d1321b057ad97c8616cff
eebann <ebanemmanuel@live.com>
2022-01-01 07:31:52 -0600

    route added

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require ('express');
 const app = express();
+const task = require('./routes/tasks');
 
 
 app.get('/', (req, res) => {
-	res.send('task manager');
-}
+	res.send('task manager');}
 );
+
+app.use('/api/v1/tasks',task);
+
+
 app.listen(3100, () => {console.log('server listening')}
 )
