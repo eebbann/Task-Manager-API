22a6644624a2e1bc667bc2543698994f13a83e23
eebann <ebanemmanuel@live.com>
2022-01-01 18:56:09 -0600

    installed mongoose

--- routes/tasks.js
+++ routes/tasks.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const {getAllTasks,createTask,getTask,updateTask,deleteTask} = require('../controllers/tasks');
 
 router.route('/').get(getAllTasks).post(createTask);
-router.route('/:id').get(getTask).put(updateTask).delete(deleteTask);
+router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask);
  
 module.exports = router;
