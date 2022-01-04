e0954b41172d24d09b11f19d99b83a056f3c9f16
eebann <ebanemmanuel@live.com>
2022-01-04 14:39:13 -0600

    created model filter for the db to retrieve json data

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,10 +1,22 @@
 const Task = require('../models/Task')
-const getAllTasks = (req, res) => {
-	res.send('get all tasks here');
-}
+
+//connects to the router 
+const getAllTasks = async (req, res) => {
+	try{
+  	const task = await Task.find({ })
+		res.status(200).json({task});
+	}catch(error){
+    res.status(500).json({msg:error})
+	}
+} 
 const createTask = async(req, res) => {
-	const task = await Task.create(req.body)
+	try{
+  	const task = await Task.create(req.body)
 		res.status(201).json({task});
+	}catch(error){
+    res.status(500).json({msg:error})
+	}
+
 }
 const getTask = (req, res) => {
 	res.json({id:req.params.id});
@@ -18,5 +30,4 @@ const deleteTask = (req, res) => {
 
 module.exports = {
 	getAllTasks, createTask, getTask, updateTask, deleteTask
-}
-// Compare this snippet from task_API/routes/tasks.js:
+} 
--- models/Task.js
+++ models/Task.js
@@ -1,7 +1,15 @@
 const mongoose = require ('mongoose');
 
 const TaskSchema = new mongoose.Schema({
-  name : String,completed:Boolean,
+  name : {
+		type: String,
+		required : [true, "set a name for your task"],
+		trim: true,
+		maxlength : [20, "should not be more than 20"],
+	},completed:{
+		type:Boolean,
+		required: false
+	}
 })
 
 module.exports = mongoose.model('Task', TaskSchema)
